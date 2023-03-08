65e10ba6824b6f8daafa9842e48ab79b6b54c650
Islamov Denis <islamov.denis@gmail.com>
2023-03-09 01:09:07 +0800

    forget to save some files

---
+++ Assets/Scripts/TonPlay.Client.Common/Network/Response/GameSessionResponse.cs
@@ -0,0 +1,10 @@
+namespace TonPlay.Client.Common.Network
+{
+    [System.Serializable]
+    public class GameSessionResponse 
+    {
+        public string id;
+        public string status;
+        public bool pve;
+    }
+}
