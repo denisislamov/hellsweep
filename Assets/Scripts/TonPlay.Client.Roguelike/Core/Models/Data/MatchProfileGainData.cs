565ad2b8b27b9acb110cdf6669556a6bba744880
Denis Islamov <di@fantasyinvest.club>
2023-03-21 21:40:42 +0800

    Add reward to models. Get and Set it

--- Assets/Scripts/TonPlay.Client.Roguelike/Core/Models/Data/MatchProfileGainData.cs
+++ Assets/Scripts/TonPlay.Client.Roguelike/Core/Models/Data/MatchProfileGainData.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace TonPlay.Client.Roguelike.Core.Models.Data
 {
 	public class MatchProfileGainData
 	{
 		public int Gold { get; set; }
 		public float ProfileExperience { get; set; }
+		
+		public List<string> ChestsId { get; set; }
 	}
 }
