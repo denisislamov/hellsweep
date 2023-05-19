d2943be4a173522defaf1d7dfe01142926349ae4
Maksim Statnykh <ms@fantasyinvest.club>
2023-05-18 18:15:45 -0700

    [DEV] Fix user locations loading

--- Assets/Scripts/TonPlay.Client.Roguelike/Profile/UserLocationsLoadingService.cs
+++ Assets/Scripts/TonPlay.Client.Roguelike/Profile/UserLocationsLoadingService.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using Cysharp.Threading.Tasks;
+using TonPlay.Client.Roguelike.Core.Locations.Interfaces;
 using TonPlay.Client.Roguelike.Inventory.Configs.Interfaces;
 using TonPlay.Client.Roguelike.Models;
 using TonPlay.Client.Roguelike.Models.Data;
@@ -13,21 +15,49 @@ namespace TonPlay.Client.Roguelike.Profile
 	public class UserLocationsLoadingService : IUserLoadingService
 	{
 		private readonly IMetaGameModelProvider _metaGameModelProvider;
-		private readonly IProfileConfigProvider _profileConfigProvider;
+		private readonly ILocationConfigProvider _locationConfigProvider;
 		private readonly IRestApiClient _restApiClient;
 		
 		public UserLocationsLoadingService(
 			IMetaGameModelProvider metaGameModelProvider,
-			IProfileConfigProvider profileConfigProvider,
+			ILocationConfigProvider locationConfigProvider,
 			IRestApiClient restApiClient)
 		{
 			_metaGameModelProvider = metaGameModelProvider;
-			_profileConfigProvider = profileConfigProvider;
+			_locationConfigProvider = locationConfigProvider;
 			_restApiClient = restApiClient;
 		}
 		
 		public async UniTask Load()
 		{
+			var userLocationsResponse = await _restApiClient.GetUserLocations();
+			var metaGameModel = _metaGameModelProvider.Get();
+			
+			var locationsModel = metaGameModel.LocationsModel;
+			var locationsData = locationsModel.ToData();
+			
+			locationsData.Locations.Clear();
+
+			var locationConfigs = _locationConfigProvider.Configs;
+
+			for (var i = 0; i < locationConfigs.Length; i++)
+			{
+				var locationConfig = locationConfigs[i];
+				var userLocation = userLocationsResponse.response.items.FirstOrDefault(_ => _.chapter == locationConfig.ChapterIdx);
+				var userLocationSurviveMills = Convert.ToDouble(userLocation?.surviveMills ?? 0L);
+				var userLocationFinished = userLocationSurviveMills >= TimeSpan.FromMinutes(14.99d).TotalMilliseconds;
+				var locationData = new LocationData()
+				{
+					ChapterIdx = locationConfig.ChapterIdx,
+					LongestSurvivedMillis = userLocationSurviveMills,
+					Unlocked = !(userLocation is null),
+					Won = userLocationFinished
+				};
+				
+				locationsData.Locations.Add(locationData.ChapterIdx, locationData);
+			}
+			
+			locationsModel.Update(locationsData);
 		}
 		
 		public class Factory : PlaceholderFactory<UserLocationsLoadingService>
